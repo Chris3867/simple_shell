5e7ff8efe2fbe6b3aa2f43f94ef27bd6980fcc70
Chris3867 <chrismulwah@gmail.com>
2022-08-16 11:34:41 -0700

    Updates on several files

--- hsh.c
+++ hsh.c
@@ -47,7 +47,7 @@ int hsh(info_t *info, char **av)
  *    Return: -1 if builtin not found,
  *             0 if  builtin  executed successfully,
  *             1 if builtin found but not successful,
- *     	-2 if builtin signals exit(
+ * -2 if builtin signals exit(
  */
 
 
@@ -76,9 +76,10 @@ int find_builtin(info_t *info)
 }
 /**
  * find_cmd - finds a command in PATH
- *   * @info: the parameter & return info struct
- ** Return: void
-  */
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
  void find_cmd(info_t *info)
  {
 	char *path = NULL;
